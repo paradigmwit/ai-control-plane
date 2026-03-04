e6630911b5dad268fd9598b7e37e673199bf340e
fahdmkhan <fahd_khan_01@hotmail.com>
2026-03-04 02:44:26 +0100

    Spring boot project skeleton

---
+++ ai-control-plane-springboot/src/main/java/com/fahdkhan/aicontrolplane/AiControlPlaneApplication.java
@@ -0,0 +1,13 @@
+package com.fahdkhan.aicontrolplane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiControlPlaneApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(AiControlPlaneApplication.class, args);
+    }
+
+}
---
+++ ai-control-plane-springboot/src/test/java/com/fahdkhan/aicontrolplane/AiControlPlaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fahdkhan.aicontrolplane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiControlPlaneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
