55fb4395fba7575587e8a790e40c1c2f57a214b9
fahdmkhan <fahd_khan_01@hotmail.com>
2026-03-18 00:13:50 +0100

    workflow executes

--- ai-control-plane-springboot/src/main/java/com/fahdkhan/aicontrolplane/security/SecurityConfig.java
+++ ai-control-plane-springboot/src/main/java/com/fahdkhan/aicontrolplane/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -26,7 +27,8 @@ public class SecurityConfig {
                         .requestMatchers("/api/user/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .httpBasic(Customizer.withDefaults());
+                .httpBasic(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
